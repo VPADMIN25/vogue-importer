ad1bdbf5621c0c100a933cd7567a47f7868926e1
VPADMIN25 <info@voguepremiere.com>
2025-11-06 22:14:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-// Ez a fájl a DigitalOcean Health Check sikeres teljesítéséhez szükséges.
-require_once 'checkinfo.php';
+// DigitalOcean Health Check file.
+// This script MUST return a 200 OK status to pass deployment.
+echo "Health check OK. Application is running.";
+http_response_code(200);
 ?>
