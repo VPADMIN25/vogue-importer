1e5742962092f367baeb2e566fe7cdef49df189d
VPADMIN25 <info@voguepremiere.com>
2025-11-06 23:24:24 +0100

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// DigitalOcean Health Check file.
-// This script MUST return a 200 OK status to pass deployment.
-echo "Health check OK. Application is running.";
-http_response_code(200);
-?>
